678860600688cc2a1f63262ab866cf8a4b01a5a8
Jiaqi Gu <919659233@qq.com>
2020-09-29 18:42:54 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -1,7 +1,17 @@
 const aCar = {
     owner : "Joe Bloggs",
-    type : 'Toyota Corolla 1.8',
-    registration : '201WD1058'
+    type : {
+        make : "Toyota",
+        nodel : "Corolla",
+        cc : 1.8
+    },
+    registration : {
+        year : 201,
+        countryCode : "WD",
+        number : 1058
+    }
 };
 
 console.log(aCar.owner);
+console.log(aCar.owner + ' drives a ' + aCar.type.make)
+console.log('Reg. = '+aCar.registration.year + '-' + aCar.registration.countryCode+ '-' +aCar.registration.number)
