b060ee3a3a27097b8015e8abee94d08af3087851
Jiaqi Gu <919659233@qq.com>
2020-09-29 18:43:40 +0100

    Dynamic properties

--- fundamentals.js
+++ fundamentals.js
@@ -13,5 +13,15 @@ const aCar = {
 };
 
 console.log(aCar.owner);
-console.log(aCar.owner + ' drives a ' + aCar.type.make)
-console.log('Reg. = '+aCar.registration.year + '-' + aCar.registration.countryCode+ '-' +aCar.registration.number)
+console.log(aCar.owner + ' drives a ' + aCar.type.make);
+console.log('Reg. = '+aCar.registration.year + '-' + aCar.registration.countryCode+ '-' +aCar.registration.number);
+aCar.mileage = 10000;
+aCar.color = {
+    exteriorColor : "red",
+    interiorFabric :{
+        texture : leather,
+        shade : cream
+    }
+
+};
+console.log("It is a "+ aCar.color.exteriorColor + " car, "+ aCar.mileage+ "mileage, with "+ aCar.color.interiorFabric.texture +" interior.");
